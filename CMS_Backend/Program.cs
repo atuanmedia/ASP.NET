79331b8fd8a81edc453b6f5838d016dfd7fc8cbd
Nguyen Duy Anh Tuan <tuan04112005@gmail.com>
2026-06-08 17:51:20 +0700

    Hoan thanh bai tap buoi 7

--- CMS_Backend/Program.cs
+++ CMS_Backend/Program.cs
@@ -36,6 +36,18 @@ builder.Services.AddCors(options => {
               .AllowAnyHeader();
     });
 });
+// ---- CẤU HÌNH CORS (THÊM VÀO TRƯỚC builder.Build()) ----
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000") // Cho phép ReactJS ở port 3000 gọi tới
+              .AllowAnyHeader()                     // Cho phép mọi loại Header (Content-Type, Authorization...)
+              .AllowAnyMethod()                     // Cho phép mọi phương thức HTTP (GET, POST, PUT, DELETE)
+              .AllowCredentials();                  // Hỗ trợ truyền Cookie/Session nếu cần sau này
+    });
+});
+
 
 var app = builder.Build();
 
@@ -53,6 +65,8 @@ app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+// Kích hoạt CORS đúng vị trí này
+app.UseCors("AllowReactApp");
 
 app.UseRouting();
 // 2. Kích hoạt chính sách CORS đã khai báo ở trên
